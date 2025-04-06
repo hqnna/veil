76cbd753f0f306d6a257ef05c137127420069a8e
hanna <me@hanna.lol>
2025-04-06 16:16:51 -0400

    build: set default abi on linux to use musl

--- build.zig
+++ build.zig
@@ -1,8 +1,11 @@
 const std = @import("std");
+const t = @import("builtin").target;
 
 pub fn build(b: *std.Build) anyerror!void {
-    const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{ .default_target = .{
+        .abi = if (t.os.tag == .linux) .musl else null,
+    } });
 
     const exe = b.addExecutable(.{
         .root_source_file = b.path("src/main.zig"),
