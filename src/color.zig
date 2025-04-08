31c4a8b095e47488f9013cab2ac76adb5290f0f8
hanna <me@hanna.lol>
2025-04-08 14:14:31 -0400

    refactor: add a command line flag for color also

--- src/color.zig
+++ src/color.zig
@@ -5,7 +5,11 @@ const ansi = @import("ansi");
 var use_color: bool = undefined;
 
 /// Check terminal capability for the use of ansi color codes
-pub fn checkCapability(allocator: std.mem.Allocator, term: std.fs.File) !void {
+pub fn checkCapability(
+    allocator: std.mem.Allocator,
+    terminal: std.fs.File,
+    cli_flag: bool,
+) !void {
     var env = try std.process.getEnvMap(allocator);
     defer env.deinit();
 
@@ -13,6 +17,6 @@ pub fn checkCapability(allocator: std.mem.Allocator, term: std.fs.File) !void {
         if (env.get("NO_COLOR")) |_| break :value false;
         if (env.get("HYPER_COLOR")) |_| break :value true;
         if (env.get("TERM")) |v| break :value !std.mem.eql(u8, v, "dumb");
-        break :value std.posix.isatty(term.handle);
+        break :value cli_flag or std.posix.isatty(terminal.handle);
     };
 }
