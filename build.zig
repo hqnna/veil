45da03d1ab428293118a60ba1ef8f6234b24d0a2
hanna <me@hanna.lol>
2025-04-08 20:51:14 -0400

    build: add known-folders to dependencies

--- build.zig
+++ build.zig
@@ -22,8 +22,10 @@ pub fn build(b: *std.Build) anyerror!void {
 
     const args = b.dependency("args", .{});
     const ansi = b.dependency("ansi_term", .{});
+    const folders = b.dependency("known_folders", .{});
     exe.root_module.addImport("args", args.module("args"));
     exe.root_module.addImport("ansi", ansi.module("ansi_term"));
+    exe.root_module.addImport("folders", folders.module("known-folders"));
     exe.root_module.addImport("zon", zon);
     b.installArtifact(exe);
 }
