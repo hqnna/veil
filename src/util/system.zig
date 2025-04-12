a100a437e890d07ed25d30b6e908352bddbbe4a8
hanna <me@hanna.lol>
2025-04-11 20:13:45 -0400

    refactor: create a filesystem utility for files

---
+++ src/util/system.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+
+/// Possible filesystem related errors for utils
+pub const Error = std.mem.Allocator.Error ||
+    std.fs.Dir.RealPathError ||
+    std.fs.File.SeekError ||
+    std.fs.File.OpenError ||
+    std.fs.File.ReadError ||
+    error{NotFile};
+
+/// Utility struct for files
+pub const File = struct {
+    data: []const u8,
+    meta: struct {
+        path: []const u8,
+        name: []const u8,
+        dir: []const u8,
+    },
+
+    /// Attempt to fetch a file from the filesystem and return info about it
+    pub fn load(allocator: std.mem.Allocator, path: []const u8) Error!File {
+        const r_path = try std.fs.cwd().realpathAlloc(allocator, path);
+        errdefer allocator.free(r_path);
+
+        const info = try std.fs.cwd().statFile(path);
+        if (info.kind != .file) return Error.NotFile;
+
+        const data = try std.fs.cwd().readFileAlloc(allocator, r_path, info.size);
+        errdefer allocator.free(data);
+
+        return File{
+            .data = data,
+            .meta = .{
+                .name = std.fs.path.basename(r_path),
+                .dir = std.fs.path.dirname(r_path).?,
+                .path = r_path,
+            },
+        };
+    }
+
+    /// Destroy the allocated path buffer for the file and other things
+    pub fn unload(file: File, allocator: std.mem.Allocator) void {
+        allocator.free(file.meta.path);
+        allocator.free(file.data);
+    }
+};
